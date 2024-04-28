955bed67c3778074240c33c4a32293705e3695d3
Paul <pasha.makey@gmail.com>
2024-04-28 20:45:34 +0300

    Initial Commit

---
+++ SnakeSUI/SnakeSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeSUIApp.swift
+//  SnakeSUI
+//
+//  Created by Paul Makey on 28.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
